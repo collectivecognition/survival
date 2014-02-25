c061892648bc7674143f25eef75973647c8d152f
William Blanchette <wb@collectivecognition.com>
2014-02-25 09:19:40 -0500

    Added eggs

---
+++ Assets/Scripts/EggWhole.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EggWhole : MonoBehaviour {
+	private float breakVelocity = 5;
+
+	public Transform half;
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		Debug.Log ("Collision");
+		if(collision.relativeVelocity.magnitude > breakVelocity){
+			Instantiate (half, transform.position + Random.insideUnitSphere, transform.rotation);
+			Instantiate (half, transform.position + Random.insideUnitSphere, transform.rotation);
+			Destroy (gameObject);
+		}
+	}
+}
